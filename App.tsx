df15ce47bae55e9b30dbb313f392765514adff22
DylanAkp <59843812+DylanAkp@users.noreply.github.com>
2024-12-12 13:02:16 +0100

    OuterTune: Initial rewrite commit using React Native

---
+++ App.tsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react';
+import { charts } from 'ytmusic_api_unofficial';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+function Section({ children, title }: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}
+      >
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}
+      >
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): React.JSX.Element {
+  const [chartData, setChartData] = useState<string>('');
+  const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    
+    charts()
+      .then((data) => {
+        setChartData(JSON.stringify(data, null, 2));
+      })
+      .catch((error) => {
+        console.error(error);
+        setChartData(`Error: ${error.message}`);
+      });
+  }, []);
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}
+      >
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}
+        >
+          <Section title="Charts">
+            <Text>{chartData}</Text>
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,68 @@
+# <img src="res/icon.png" alt="OuterTune Icon" width="35" height="35" style="vertical-align: middle;"> OuterTune
+ 
+<div align="center">
+    <img src="res/screenshot.png" alt="OuterTune Screenshot" width="700">
+</div>
+
+OuterTune is a music player for mobile and desktop with a focus on design and functionality, OuterTune aims to be a solid alternative to your streaming services without any ads.
+
+<div align="center">
+    <a href="https://github.com/DylanAkp/OuterTune/releases/latest">
+        <img src="res/download.png" alt="Download Button" width="200">
+    </a>
+</div>
+
+---
+
+# 🚀 Getting Started
+
+## Step 1: Install Dependencies
+
+Make sure you have completed the [React Native Environment Setup](https://reactnative.dev/docs/environment-setup) for your operating system before proceeding.
+
+1. Install dependencies:
+   ```bash
+   npm install
+   ```
+
+## Step 2: Start the Metro Server
+
+Metro is the JavaScript bundler that ships with React Native. To start it, run the following command from the root of the OuterTune project:
+
+```bash
+npm start
+```
+
+## Step 3: Run the Application
+
+Let Metro Bundler run in its own terminal. Open a new terminal and run one of the following commands:
+
+### For Android:
+
+```bash
+npm run android
+```
+
+### For iOS:
+
+```bash
+npm run ios
+```
+
+If everything is set up correctly, you should see OuterTune running on your Android Emulator or iOS Simulator. 📱 You can also run it directly from Android Studio or Xcode if needed.
+
+---
+
+# 🙌 Credits
+
+- [Alexis06030631](https://github.com/Alexis06030631/) for his [YouTube Music API](https://github.com/Alexis06030631/yt_music_api)
+- [FontAwesome](http://fontawesome.io) by Dave Gandy
+
+---
+
+# 📜 Licenses
+
+- **FontAwesome**: [SIL OFL 1.1](https://openfontlicense.org/)
+- **OuterTune**: [MIT License](https://opensource.org/license/mit)
+
+---
